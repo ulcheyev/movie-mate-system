dd985a0f591193923a7a3af3e19a740dd066ba6c
Yevgeniy <ulchenkov2001@gmail.com>
2024-10-20 22:25:07 +0200

    [INIT] readme added. Project structure initialized.

---
+++ recommendation-service/src/test/java/cz/cvut/recommendationservice/RecommendationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.cvut.recommendationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecommendationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ watchlist-service/src/main/java/cz/cvut/watchlistservice/WatchlistServiceApplication.java
@@ -0,0 +1,13 @@
+package cz.cvut.watchlistservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WatchlistServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WatchlistServiceApplication.class, args);
+    }
+
+}
